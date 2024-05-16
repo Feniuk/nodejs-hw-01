734a23e07e967c1f7960eedf6a58dba804cd0801
Feniuk <143640539+Feniuk@users.noreply.github.com>
2024-05-16 22:00:32 +0200

    generateContacts done

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,4 +1,6 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
 export const addOneContact = async () => {};
 
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,22 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import fs from 'fs/promises';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  try {
+    const contacts = JSON.parse(await fs.readFile(PATH_DB, 'utf-8'));
+
+    const updatedContacts = [];
+    for (let i = 0; i < number; i += 1) {
+      const newContact = createFakeContact();
+      updatedContacts.push(newContact);
+    }
+
+    contacts.push(...updatedContacts);
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+  } catch (err) {
+    console.log(err, 'Something went wrong');
+  }
+};
 
 await generateContacts(5);
